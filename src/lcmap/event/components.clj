a9a7c960519b2f12b7a357691465c84402ed2192
Duncan McGreggor <oubiwann@gmail.com>
2016-05-18 10:28:08 -0500

    Updated to ditch var bindings and fixed restart.

--- src/lcmap/event/components.clj
+++ src/lcmap/event/components.clj
@@ -52,14 +52,19 @@
             :msging])))
 
 (defn stop [system component-key]
-  (let [updated-component (component/stop (component-key system))]
-    (assoc system component-key updated-component)))
+  (->> system
+       (component-key)
+       (component/stop)
+       (assoc system component-key)))
 
 (defn start [system component-key]
-  (let [updated-component (component/start (component-key system))]
-    (assoc system component-key updated-component)))
+  (->> system
+       (component-key)
+       (component/start)
+       (assoc system component-key)))
 
 (defn restart [system component-key]
   (-> system
-      (stop)
-      (start)))
+      (stop component-key)
+      (start component-key)))
+
