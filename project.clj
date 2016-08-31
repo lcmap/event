47a4a89e36e338fe0190c24ccd6d7d9178f0f08f
Duncan McGreggor <oubiwann@gmail.com>
2016-08-30 21:54:44 -0500

    Use parent manged deps.

--- project.clj
+++ project.clj
@@ -1,27 +1,39 @@
 (defproject gov.usgs.eros/lcmap-event "1.0.0-SNAPSHOT"
+  :parent-project {
+    :coords [gov.usgs.eros/lcmap-system "1.0.0-SNAPSHOT"]
+    :inherit [
+      :deploy-repositories
+      :license
+      :managed-dependencies
+      :plugins
+      :pom-addition
+      :repositories
+      :target
+      ;; XXX The following can be un-commented once this issue is resolved:
+      ;;     * https://github.com/achin/lein-parent/issues/3
+      ;; [:profiles [:uberjar :dev]]
+      ]}
   :description "LCMAP Event System"
   :url "https://github.com/USGS-EROS/lcmap-event"
-  :license {:name "NASA Open Source Agreement, Version 1.3"
-            :url "http://ti.arc.nasa.gov/opensource/nosa/"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/core.match "0.3.0-alpha4"]
-                 [org.clojure/data.codec "0.1.0"]
-                 [org.clojure/data.json "0.2.6"]
-                 [org.clojure/data.xml "0.0.8"]
-                 [org.clojure/core.memoize "0.5.9"]
+  :dependencies [[org.clojure/clojure]
+                 [org.clojure/core.match]
+                 [org.clojure/data.codec]
+                 [org.clojure/data.json]
+                 [org.clojure/data.xml]
+                 [org.clojure/core.memoize]
                  ;; Componentization
-                 [com.stuartsierra/component "0.3.1"]
+                 [com.stuartsierra/component]
                  ;; Logging and Error Handling -- note that we need to explicitly pull
                  ;; in a version of slf4j so that we don't get conflict messages on the
                  ;; console
-                 [dire "0.5.4"]
-                 [slingshot "0.12.2"]
+                 [dire]
+                 [slingshot]
                  ;; Messaging
-                 [com.novemberain/langohr "3.6.1"]
+                 [com.novemberain/langohr]
                  ;; LCMAP Components
-                 [gov.usgs.eros/lcmap-config "1.0.0-SNAPSHOT"]
-                 [gov.usgs.eros/lcmap-logger "1.0.0-SNAPSHOT"]
-                 [gov.usgs.eros/lcmap-client-clj "1.0.0-SNAPSHOT"]
+                 [gov.usgs.eros/lcmap-config]
+                 [gov.usgs.eros/lcmap-logger]
+                 [gov.usgs.eros/lcmap-client-clj]
                  ;; XXX note that we may still need to explicitly include the
                  ;; Apache Java HTTP client, since the version used by the LCMAP
                  ;; client is more recent than that used by Chas Emerick's
@@ -29,25 +41,21 @@
                  ;; is worked around by explicitly including Apache Java HTTP
                  ;; client library).
                  ;; XXX temp dependencies:
-                 [org.apache.httpcomponents/httpclient "4.5.2"]
-                 [clojure-ini "0.0.2"]
-                 [clj-http "3.1.0"]
+                 [org.apache.httpcomponents/httpclient]
+                 [clojure-ini]
+                 [clj-http]
                  ;; Data types, encoding, etc.
-                 [byte-streams "0.2.2"]
-                 [clj-time "0.12.0"]
-                 [commons-codec "1.10"]
+                 [byte-streams]
+                 [clj-time]
+                 [commons-codec]
                  ;; Dev and project metadata
-                 [leiningen-core "2.6.1"]]
-  :plugins [[lein-ring "0.9.7"]
-            [lein-pprint "1.1.2"]
-            [lein-codox "0.9.5"]
-            [lein-simpleton "1.3.0"]]
+                 [leiningen-core]]
+  :plugins [[lein-parent "0.3.0"]]
   :source-paths ["src" "test/support/auth-server/src"]
   :java-agents [[co.paralleluniverse/quasar-core "0.7.3"]]
   :jvm-opts ["-Dco.paralleluniverse.fibers.detectRunawayFibers=false"]
   :repl-options {:init-ns lcmap.event.dev}
   :main lcmap.event.app
-  :target-path "target/%s"
   :codox {:project {:name "lcmap.event"
                     :description "The Event Subscription and Notification Component for the USGS Land Change Monitoring Assessment and Projection (LCMAP) Computation and Analysis Platform"}
           :namespaces [#"^lcmap.event\."]
